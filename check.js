d823e15ed60373b619c912ed7634102fdc869c4c
Mohammad Mahdi Moodi <83002648+moodi2005@users.noreply.github.com>
2021-09-20 08:11:58 +0430

    Update check.js

--- check.js
+++ check.js
@@ -6,8 +6,8 @@ module.exports = (domain) => {
       `http://api.whoapi.com/?apikey=4c15263b4a0c99ab2106bdfe3650c711&r=whois&domain=${domain}.ir`
     )
     .then((reqd) => {
-      if (reqd.data.registered) {
-       console.log(`${domain}.ir => ${reqd.data.registered}`);
+      if (!reqd.data.registered) {
+       console.log(`${domain}.ir => Not purchased`);
       }
     }).catch(err=>console.log("err"))
 };
